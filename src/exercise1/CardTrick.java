37bb270264d564a840e2086bf28db22e0c553fab
sukhjeet4101 <157637015+sukhjeet4101@users.noreply.github.com>
2024-02-07 12:41:13 -0500

    updated from github

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -47,19 +47,18 @@ public class CardTrick {
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
 
-        System.out.println("My name is Paul, but you can call me prof, Paul, or sir");
+        System.out.println("My name is sukhjeet singh, but you can call me prof, sukh ");
         System.out.println();
 
         System.out.println("My career ambitions:");
         System.out.println("-- Be more active on LinkedIn");
-        System.out.println("-- Have a semester with no violations of academic integrity!");
-        System.out.println();
+        System.out.println("become singer");
+        System.out.println("buy g wagon");
 
         System.out.println("My hobbies:");
         System.out.println("-- Investing");
-        System.out.println("-- Cooking");
-        System.out.println("-- Reading/Watching TV");
-        System.out.println("-- Riding my motorcycle");
+        System.out.println("singing");
+        
 
         System.out.println();
     }
